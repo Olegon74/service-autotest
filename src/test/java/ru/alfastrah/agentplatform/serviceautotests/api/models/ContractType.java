5f55bf94f3001d3c5704a8ebd1df2012f9efd374
Oleg <128599778+Olegon74@users.noreply.github.com>
2026-01-15 18:01:32 +0500

    Add tab API tests and clients

---
+++ src/test/java/ru/alfastrah/agentplatform/serviceautotests/api/models/ContractType.java
@@ -0,0 +1,10 @@
+package ru.alfastrah.agentplatform.serviceautotests.api.models;
+
+public enum ContractType {
+    PRIMARY,
+    PROLONGATION,
+    ADDITIONAL,
+    COPY,
+    CANCELED,
+    ANNUL
+}
